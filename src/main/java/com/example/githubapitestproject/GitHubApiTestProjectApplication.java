5850805a7affaddd7cc843495291e0b98c43dc20
Erik Busch <erik_busch@gmx.de>
2022-06-01 16:07:35 +0200

    initial commit

---
+++ src/main/java/com/example/githubapitestproject/GitHubApiTestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubapitestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubApiTestProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubApiTestProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/githubapitestproject/GitHubApiTestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubapitestproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubApiTestProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
